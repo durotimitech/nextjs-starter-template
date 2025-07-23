413a4e123d23959170279e12194365f7924f0d2e
timmymejabitoast <timmy.mejabi@toasttab.com>
2025-07-23 08:11:22 +0100

    added middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,19 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/middleware'
 
-export async function middleware(req: NextRequest) {
-    // Only protect /dashboard route
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        const res = NextResponse.next();
-        const supabase = createMiddlewareClient({ req, res });
-        const {
-            data: { session },
-        } = await supabase.auth.getSession();
-        if (!session) {
-            const redirectUrl = req.nextUrl.clone();
-            redirectUrl.pathname = '/signin';
-            redirectUrl.searchParams.set('redirectedFrom', req.nextUrl.pathname);
-            return NextResponse.redirect(redirectUrl);
-        }
-        return res;
-    }
-    return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
 }
 
 export const config = {
-    matcher: ['/dashboard'],
-}; 
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
